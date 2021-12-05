8e8ed68b8ab98aae0256cbe984e0147bda4351a2
Hollis Wu <whyhollis@gmail.com>
2021-12-05 12:09:36 +0800

    refactor(mpv): Refactor delete file script

--- mpv/scripts/delete_file.lua
+++ mpv/scripts/delete_file.lua
@@ -1,72 +1,115 @@
-local utils = require "mp.utils"
+local mp = require("mp")
+local utils = require("mp.utils")
 
-del_list = {}
+local M = {}
+M.__index = M
 
-function contains_item(l, i)
-   for k, v in pairs(l) do
-      if v == i then
-         mp.osd_message("undeleting current file")
-         l[k] = nil
-         return true
-      end
-   end
-   mp.osd_message("deleting current file")
-   return false
+---@class M
+---@field del_list string[]
+---@field show_list_timer any
+function M:create()
+	local obj = {} -- our new object
+	setmetatable(obj, M) -- make M handle lookup
+
+	obj.del_list = {}
+	obj.show_list_timer = mp.add_periodic_timer(1, function()
+		obj:show_list()
+	end)
+	obj.show_list_timer:kill()
+
+	return obj
 end
 
-function mark_delete()
-   local work_dir = mp.get_property_native("working-directory")
-   local file_path = mp.get_property_native("path")
-   local s = file_path:find(work_dir, 0, true)
-   local final_path
-   if s and s == 0 then
-      final_path = file_path
-   else
-      final_path = utils.join_path(work_dir, file_path)
-   end
-   if not contains_item(del_list, final_path) then
-      table.insert(del_list, final_path)
-   end
+---@param item string
+---@return boolean
+function M:contains_item(item)
+	for k, v in pairs(self.del_list) do
+		if v == item then
+			mp.osd_message("undeleting current file")
+			self.del_list[k] = nil
+			return true
+		end
+	end
+
+	mp.osd_message("deleting current file")
+	return false
 end
 
-function delete()
-   for i, v in pairs(del_list) do
-      print("deleting: "..v)
-      os.remove(v)
-   end
+--- Mark currently playing file to delete
+function M:mark_delete()
+	local work_dir = mp.get_property_native("working-directory")
+	local file_path = mp.get_property_native("path")
+	local s = file_path:find(work_dir, 0, true)
+	local final_path
+
+	if s and s == 0 then
+		final_path = file_path
+	else
+		final_path = utils.join_path(work_dir, file_path)
+	end
+
+	if not self:contains_item(final_path) then
+		table.insert(self.del_list, final_path)
+	end
 end
 
-function showList()
-   local delString = "Delete Marks:\n"
-   for _,v in pairs(del_list) do
-      local dFile = v:gsub("/","\\")
-      delString = delString..dFile:match("\\*([^\\]*)$").."; "
-   end
-   if delString:find(";") then
-      mp.osd_message(delString)
-      return delString
-   elseif showListTimer then
-      showListTimer:kill()
-   end
-end
-showListTimer = mp.add_periodic_timer(1,showList)
-showListTimer:kill()
-function list_marks()
-   if showListTimer:is_enabled() then
-      showListTimer:kill()
-      mp.osd_message("",0)
-   else
-      local delString = showList()
-      if delString and delString:find(";") then
-         showListTimer:resume()
-         print(delString)
-      else
-         showListTimer:kill()
-      end
-   end
+function M:delete()
+	for _, v in pairs(self.del_list) do
+		print("deleting: " .. v)
+		os.remove(v)
+	end
 end
 
-mp.add_key_binding("ctrl+DEL", "delete_file", mark_delete)
-mp.add_key_binding("alt+DEL", "list_marks", list_marks)
-mp.add_key_binding("ctrl+shift+DEL", "clear_list", function() mp.osd_message("Undelete all"); del_list = {}; end)
-mp.register_event("shutdown", delete)
+function M:show_list()
+	local del_string = "Delete Marks:\n"
+	for _, v in pairs(self.del_list) do
+		local dFile = v:gsub("/", "\\")
+		del_string = del_string .. dFile:match("\\*([^\\]*)$") .. "; "
+	end
+
+	if del_string:find(";") then
+		mp.osd_message(del_string)
+		return del_string
+	elseif self.show_list_timer then
+		self.show_list_timer:kill()
+	end
+end
+
+function M:list_marks()
+	if self.show_list_timer:is_enabled() then
+		self.show_list_timer:kill()
+		mp.osd_message("", 0)
+	else
+		local delString = self:show_list()
+		if delString and delString:find(";") then
+			self.show_list_timer:resume()
+			print(delString)
+		else
+			self.show_list_timer:kill()
+		end
+	end
+end
+
+function M:bind_keys()
+	mp.add_key_binding("ctrl+DEL", "delete_file", function()
+		self:mark_delete()
+	end)
+	mp.add_key_binding("META+BS", "delete_file", function()
+		self:mark_delete()
+	end) -- For MacOS
+
+	mp.add_key_binding("alt+DEL", "list_marks", function()
+		self:list_marks()
+	end)
+
+	mp.add_key_binding("ctrl+shift+DEL", "clear_list", function()
+		mp.osd_message("Undelete all")
+		self.del_list = {}
+	end)
+	mp.register_event("shutdown", function()
+		self:delete()
+	end)
+end
+
+local m = M:create()
+m:bind_keys()
