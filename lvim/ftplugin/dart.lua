b7bbcba865b84bba4921fa4afa7687801b9bab6f
Hollis Wu <whyhollis@gmail.com>
2022-04-05 09:30:00 +0800

    feat(lvim): Add flutter key in telescope

--- lvim/ftplugin/dart.lua
+++ lvim/ftplugin/dart.lua
@@ -1,4 +1,16 @@
 -- Need to wait for packer to load flutter plugin first
 vim.defer_fn(function()
-	require("telescope").load_extension("flutter")
+	local telescope = require("telescope")
+	local wk = require("which-key")
+
+	telescope.load_extension("flutter")
+
+	wk.register({
+		F = {
+			function()
+				telescope.extensions.flutter.commands()
+			end,
+			"Flutter",
+		},
+	}, { prefix = "<leader>s" })
 end, 10)
