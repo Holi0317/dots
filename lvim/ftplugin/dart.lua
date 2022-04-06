7ddb9cf8630af2170b90df55b0fb534694e745bd
Hollis Wu <whyhollis@gmail.com>
2022-04-06 10:13:59 +0800

    refactor(lvim): Change keymap for flutter

--- lvim/ftplugin/dart.lua
+++ lvim/ftplugin/dart.lua
@@ -1,16 +1,8 @@
 -- Need to wait for packer to load flutter plugin first
 vim.defer_fn(function()
 	local telescope = require("telescope")
-	local wk = require("which-key")
 
 	telescope.load_extension("flutter")
-
-	wk.register({
-		F = {
-			function()
-				telescope.extensions.flutter.commands()
-			end,
-			"Flutter",
-		},
-	}, { prefix = "<leader>s" })
 end, 10)
+
+vim.api.nvim_buf_set_keymap(0, "n", "\\", "<cmd>Telescope flutter commands<cr>", { silent = true })
