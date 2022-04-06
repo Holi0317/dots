fbe18b04199124e29142c8ae9f933afb19919e1c
Hollis Wu <whyhollis@gmail.com>
2022-04-06 10:39:22 +0800

    refactor(lvim): Use friendly snippet for flutter

--- lvim/ftplugin/dart.lua
+++ lvim/ftplugin/dart.lua
@@ -1,3 +1,6 @@
+-- Load flutter snippets
+require("luasnip").filetype_extend("dart", { "flutter" })
+
 -- Need to wait for packer to load flutter plugin first
 vim.defer_fn(function()
 	local telescope = require("telescope")
