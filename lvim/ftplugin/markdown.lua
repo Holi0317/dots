7ed48b02354dab4e284d6da0968615941287b74d
Hollis Wu <whyhollis@gmail.com>
2022-06-22 09:26:55 +0800

    [lvim] Add zk plugin

--- lvim/ftplugin/markdown.lua
+++ lvim/ftplugin/markdown.lua
@@ -1,39 +1,24 @@
--- Bind Markdown-specific keys (telekasten stuff) to \
+-- Bind Markdown-specific keys (zk stuff) to `\` key
 local wk = require("which-key")
-local context_keys = require("h4s.context_keys")
 
 local current_buf = vim.api.nvim_get_current_buf()
-wk.register({
-	name = "Telekasten",
-	n = { "<cmd>lua require('telekasten').new_note()<CR>", "New note" },
-	N = { "<cmd>lua require('telekasten').new_templated_note()<CR>", "New note from template" },
-	t = { "<cmd>lua require('telekasten').goto_today()<CR>", "Goto today" },
-	c = { "<cmd>lua require('telekasten').show_calendar()<CR>", "Show calendar" },
-	C = { "<cmd>CalendarT<CR>", "Show big calendar" },
-	z = { "<cmd>lua require('telekasten').follow_link()<CR>", "Follow link" },
-	b = { "<cmd>lua require('telekasten').show_backlinks()<CR>", "Show backlink" },
-	f = { "<cmd>lua require('telekasten').find_friends()<CR>", "Find friends" },
-	["["] = { "<cmd>lua require('telekasten').insert_link()<CR>", "Insert link" },
-}, {
-	prefix = "\\",
-	buffer = current_buf,
-})
-
-vim.api.nvim_buf_set_keymap(
-	0,
-	"i",
-	"\\[",
-	"<ESC>:lua require('telekasten').insert_link({ i=true })<CR>",
-	{ silent = true }
-)
-
-context_keys.force_buf_remap(
-	current_buf,
-	"",
-	"gd",
-	"<cmd>lua require('telekasten').follow_link()<CR>",
-	{ silent = true, noremap = true }
-)
 
 -- Show color column at 90
 vim.bo.textwidth = 90
+
+vim.schedule(function()
+	local zkutil = require("zk.util")
+
+	if zkutil.notebook_root(vim.fn.expand("%:p")) == nil then
+		return
+	end
+
+	wk.register({
+		name = "zk",
+		n = { "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", "New note" },
+		o = { "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", "Open note" },
+	}, {
+		prefix = "\\",
+		buffer = current_buf,
+	})
+end)
