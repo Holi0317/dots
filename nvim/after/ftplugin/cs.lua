9c5eaf995ab9f19e844d1ae65aa29b7b597836d6
Hollis Wu <whyhollis@gmail.com>
2022-10-09 00:53:31 +0800

    [nvim] Fix `gd` not working in csharp

--- nvim/after/ftplugin/cs.lua
+++ nvim/after/ftplugin/cs.lua
@@ -7,5 +7,16 @@ require("user.lsp").setup("omnisharp", {
 		handlers = {
 			["textDocument/definition"] = require("omnisharp_extended").handler,
 		},
+		on_attach = function(client, bufnr)
+			require("user.lsp.callbacks").on_attach(client, bufnr)
+
+			-- Override `gd` to **not** use trouble. Otherwise it breaks omnisharp_extended
+			vim.keymap.set("n", "gd", function()
+				vim.lsp.buf.definition()
+			end, {
+				desc = "Goto Definition",
+				buffer = bufnr,
+			})
+		end,
 	},
 })
