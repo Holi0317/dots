88ab4fa9e9303e552d20f07ca68fbd86be9a9885
Hollis Wu <whyhollis@gmail.com>
2022-10-09 10:49:44 +0800

    [nvim] Fix format not working in csharp

--- nvim/after/ftplugin/cs.lua
+++ nvim/after/ftplugin/cs.lua
@@ -3,11 +3,15 @@ vim.bo.softtabstop = 4
 vim.bo.shiftwidth = 4
 
 require("user.lsp").setup("omnisharp", {
+	enable_format = true,
 	override = {
 		handlers = {
 			["textDocument/definition"] = require("omnisharp_extended").handler,
 		},
 		on_attach = function(client, bufnr)
+			-- Enable formatting
+			require("lsp-format").on_attach(client)
+
 			require("user.lsp.callbacks").on_attach(client, bufnr)
 
 			-- Override `gd` to **not** use trouble. Otherwise it breaks omnisharp_extended
