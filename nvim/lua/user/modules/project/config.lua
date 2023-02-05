0aa0e0c403f3922ba380957b795a5396cdddc37e
Hollis Wu <whyhollis@gmail.com>
2023-02-05 17:54:18 +0800

    [nvim] Tweak root detection for project

--- nvim/lua/user/modules/project/config.lua
+++ nvim/lua/user/modules/project/config.lua
@@ -2,6 +2,8 @@ local M = {}
 
 function M.project()
 	require("project_nvim").setup({
+		detection_methods = { "pattern" },
+		patterns = { ".git", ".obsidian" },
 		ignore_lsp = {
 			"null-ls",
 		},
