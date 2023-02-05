33662787aa02c82f95ba03ad10d72492e9a8a70e
Hollis Wu <whyhollis@gmail.com>
2023-02-05 17:53:19 +0800

    [nvim] Add trouble

--- nvim/lua/user/keys.lua
+++ nvim/lua/user/keys.lua
@@ -116,6 +116,16 @@ local M = {
 				"Notifications",
 			},
 		},
+		t = {
+			name = "+Trouble",
+			r = { "<cmd>Trouble lsp_references<cr>", "References" },
+			f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+			d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
+			q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+			l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+			w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
+			t = { "<cmd>TodoTrouble<cr>", "Todo" },
+		}
 	},
 }
 
