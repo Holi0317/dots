b6e65f86dd06c60a8e71c2597a465c9d08acda26
Hollis Wu <whyhollis@gmail.com>
2024-07-17 21:22:53 -0400

    [nvim] Fix conflicting keymap gs

--- nvim/lua/user/modules/ui.lua
+++ nvim/lua/user/modules/ui.lua
@@ -146,7 +146,6 @@ return {
 		config = function()
 			vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
 			vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
-			vim.keymap.set({ "n", "x", "o" }, "gs", "<Plug>(leap-from-window)")
 		end,
 	},
 
