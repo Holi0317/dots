fc89fc1b2df4abacb8387883f81b530b284eb182
Hollis Wu <whyhollis@gmail.com>
2024-12-31 14:53:54 -0500

    [nvim] Add toggleterm binding

--- nvim/lua/user/modules/term.lua
+++ nvim/lua/user/modules/term.lua
@@ -1,11 +1,18 @@
+---Setup terminal keymaps
+---This will get called by ToggleTerm when a terminal is opened, I guess?
+function _G.set_terminal_keymaps()
+	local opts = { buffer = 0 }
+	vim.keymap.set("t", "<C-j>", [[<C-\><C-n>]], opts)
+end
+
 return {
 	{
 		"akinsho/toggleterm.nvim",
 		version = "*",
 		lazy = true,
-		keys = { [[<c-\>]] },
+		keys = { [[<c-\>]], "<c-j>" },
 		opts = {
-			open_mapping = [[<c-\>]],
+			open_mapping = { [[<c-\>]], "<c-j>" },
 		},
 	},
 	{
