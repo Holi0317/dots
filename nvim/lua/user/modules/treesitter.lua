4567d278065bfedf647554324ba243032a855ecd
Hollis Wu <whyhollis@gmail.com>
2025-09-08 20:16:11 -0400

    [nvim] Upgrade treesitter to main branch

--- nvim/lua/user/modules/treesitter.lua
+++ nvim/lua/user/modules/treesitter.lua
@@ -1,168 +1,154 @@
--- JoosepAlviste/nvim-ts-context-commentstring: Skip compatibility check
-vim.g.skip_ts_context_commentstring_module = true
-
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
-			"windwp/nvim-ts-autotag",
-			"nvim-treesitter/playground",
-			"theHamsta/nvim-treesitter-pairs",
-			"adelarsq/vim-matchit", -- Required by pairs
-			"nvim-treesitter/nvim-treesitter-context",
-			"JoosepAlviste/nvim-ts-context-commentstring",
-		},
+		branch = "main",
 		config = function()
 			vim.treesitter.language.register("markdown", "mdx")
 
-			require("nvim-treesitter.configs").setup({
-				-- A list of parser names, or "all"
-				ensure_installed = {
-					-- vim
-					"lua",
-					"luadoc",
-					"vim",
-					"vimdoc",
+			require("nvim-treesitter").install({
+				-- vim
+				"lua",
+				"luadoc",
+				"vim",
+				"vimdoc",
 
-					-- MD
-					"markdown",
-					"markdown_inline",
-					"mermaid",
+				-- MD
+				"markdown",
+				"markdown_inline",
+				"mermaid",
 
-					-- Webs
-					"javascript",
-					"jsdoc",
-					"typescript",
-					"tsx",
-					"vue",
-					"html",
-					"css",
-					"scss",
-					"prisma",
-					"astro",
+				-- Webs
+				"javascript",
+				"jsdoc",
+				"typescript",
+				"tsx",
+				"vue",
+				"html",
+				"css",
+				"scss",
+				"prisma",
+				"astro",
 
-					-- Config languages
-					"json",
-					"json5",
-					"jsonc",
-					"yaml",
-					"toml",
-					"dockerfile",
-					"hcl",
-					"terraform",
-					"scheme",
-					"ini",
+				-- Config languages
+				"json",
+				"json5",
+				"jsonc",
+				"yaml",
+				"toml",
+				"dockerfile",
+				"hcl",
+				"terraform",
+				"scheme",
+				"ini",
 
-					-- C
-					"c",
-					"cpp",
-					"cmake",
-					"make",
+				-- C
+				"c",
+				"cpp",
+				"cmake",
+				"make",
 
-					-- Go
-					"go",
-					"gomod",
+				-- Go
+				"go",
+				"gomod",
 
-					-- PHP
-					"php",
-					-- "phpdoc",  -- This is causing problem with installing on aarch darwin
+				-- PHP
+				"php",
+				-- "phpdoc",  -- This is causing problem with installing on aarch darwin
 
-					-- Git
-					"git_config",
-					"git_rebase",
-					"gitattributes",
-					"gitcommit",
-					"gitignore",
+				-- Git
+				"git_config",
+				"git_rebase",
+				"gitattributes",
+				"gitcommit",
+				"gitignore",
 
-					-- Rust
-					"rust",
+				-- Rust
+				"rust",
 
-					"comment",
-					"c_sharp",
-					"python",
-					"regex",
-					"sql",
-					"bash",
-					"nu",
-					"typst",
-				},
-
-				-- Install parsers synchronously (only applied to `ensure_installed`)
-				sync_install = false,
-
-				-- Automatically install missing parsers when entering buffer
-				auto_install = true,
-
-				---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-				-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-				-- ==== Module settings ====
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
-
-				indent = {
-					enable = true,
-					disable = {},
-				},
-
-				textobjects = {
-					swap = {
-						enable = false,
-						-- swap_next = textobj_swap_keymaps,
-					},
-					select = {
-						enable = true,
-						lookahead = true,
-						keymaps = {
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-
-							["ac"] = "@class.outer",
-							["ic"] = "@class.inner",
-
-							["a,"] = "@parameter.outer",
-							["i,"] = "@parameter.inner",
-
-							["a/"] = "@comment",
-							["i/"] = "@comment.inner",
-
-							["ax"] = "@htmlattr.outer",
-							["ix"] = "@htmlattr.inner",
-						},
-					},
-				},
-
-				autotag = {
-					enable = true,
-				},
-
-				playground = {
-					enable = true,
-				},
-
-				pairs = {
-					enable = true,
-					keymaps = {
-						goto_partner = "%",
-					},
-				},
+				"comment",
+				"c_sharp",
+				"python",
+				"regex",
+				"sql",
+				"bash",
+				"nu",
+				"typst",
 			})
 
-			require("ts_context_commentstring").setup({
-				enable_autocmd = false,
-			})
-
-			require("treesitter-context").setup({
-				max_lines = 5,
+			vim.api.nvim_create_autocmd("FileType", {
+				-- pattern = { "<filetype>" },
+				callback = function()
+					-- Ignore error if language don't have treesitter with pcall
+					pcall(vim.treesitter.start)
+				end,
 			})
 
 			-- Enable folding with treesitter
 			vim.o.foldmethod = "expr"
-			vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+			vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 			vim.o.foldlevelstart = 99
+
+			-- Indent with treesitter
+			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("nvim-treesitter-textobjects").setup({
+				select = {
+					lookahead = true,
+				},
+			})
+
+			local function mkkeymap(key, selector)
+				vim.keymap.set({ "x", "o" }, key, function()
+					require("nvim-treesitter-textobjects.select").select_textobject(selector, "textobjects")
+				end)
+			end
+
+			mkkeymap("af", "@function.outer")
+			mkkeymap("if", "@function.inner")
+
+			mkkeymap("ac", "@class.outer")
+			mkkeymap("ic", "@class.inner")
+
+			mkkeymap("a,", "@parameter.outer")
+			mkkeymap("i,", "@parameter.inner")
+
+			mkkeymap("a/", "@comment")
+			mkkeymap("i/", "@comment.inner")
+
+			mkkeymap("ax", "@htmlattr.outer")
+			mkkeymap("ix", "@htmlattr.inner")
+		end,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		branch = "main",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("nvim-ts-autotag").setup({})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("treesitter-context").setup({
+				max_lines = 5,
+			})
+		end,
+	},
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("ts_context_commentstring").setup({
+				enable_autocmd = false,
+			})
 		end,
 	},
 }
