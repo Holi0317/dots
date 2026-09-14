55ad0989e8241b044dfbdc78974922f35138dc31
Hollis Wu <whyhollis@gmail.com>
2026-09-14 10:32:44 -0400

    [nvim] Use nu as term if possible

--- nvim/lua/user/modules/term.lua
+++ nvim/lua/user/modules/term.lua
@@ -13,6 +13,22 @@ return {
 		keys = { [[<c-\>]], "<c-j>" },
 		opts = {
 			open_mapping = { [[<c-\>]], "<c-j>" },
+			-- Use nu when neovim is called under nu.
+			-- Cannot change my shell in some cases but I still want terminal in nu.
+			shell = function()
+				-- Already set to nu
+				if vim.endswith(vim.o.shell, "/nu") then
+					return vim.o.shell
+				end
+
+				-- Probably was called under nu if this env is set
+				local nu_version = os.getenv("NU_VERSION")
+				if nu_version then
+					return "nu"
+				end
+
+				return vim.o.shell
+			end,
 		},
 	},
 	{
